26d714f7049c56a40a002a8dccd0d50f8960416a
Dani Guijo <dani_guijo@hotmail.com>
2024-02-02 19:03:57 +0100

    Add methods

--- data/power_system.py
+++ data/power_system.py
@@ -7,30 +7,74 @@ class PowerSystem():
     """
 
     def __init__(self,
-                 hours=24,
-                 min_players=10
+                 Z: list,
+                 R: np.ndarray,
+                 X: np.ndarray,
+                 edges: list
                  ):
 
-        """
-        Attributes:
+        '''
+        Initializes a Power System.
 
-        hours: Number of hours during which trading is possible.
-        min_players: Minimum number of hourly transactions.
-        max_players: Maximum number of hourly transactions.
-        min_price: Minimum energy price in cents/KWh.
-        max_price: Maximum energy price in cents/KWh.
-        min_bid: Minimum value of the energy bid range.
-        max_bid: Maximum value of the energy bid range.
-        min_ask: Minimum value of the energy ask range.
-        max_ask: Maximum value of the energy ask range.
-        """
+        Z: Array of measurements (type, nodes, value, sigma)
+        
+            Type | Description | 
+            -----|------------
+            0    | Voltage
+            1    | Power Flow between two nodes (Real)
+            2    | Power Flow between two nodes (Imaginary)
+            3    | Power injection at a node (Real)
+            4    | Power injection at a node (Imaginary)
 
-        self.hours = hours
-        self.min_players = min_players
-        self.max_players = max_players
-        self.min_price = min_price
-        self.max_price = max_price
-        self.min_bid = min_bid
-        self.max_bid = max_bid
-        self.min_ask = max_bid
-        self.max_ask = 2*max_bid
+        R: Resistance Matrix
+        X: Reactance Matrix
+        edges: List of edges (i, j) where i and j are the nodes
+        ''' 
+        self.Z = Z
+        self.Z_values = np.array([value for _, _, value, _ in Z])
+        self.R = R
+        self.X = X
+        self.n_measurements = len(Z)
+        self.n_nodes = len(R)
+        self.n_edges = len(edges)
+        self.edges = edges
+        
+        self.admitance = self.get_admitance(R, X)
+        self.bus_admitance = self.get_bus_admitance(self.admitance)
+
+        self.weights = np.array([1 / sigma**2 for _, _, _, sigma in Z])
+        W = np.diag(self.weights)
+        self.W = W
+
+
+    def get_admitance(self, R, X):
+        '''
+        Get Admitance Matrix
+
+        R: Resistance Matrix
+        X: Reactance Matrix
+        '''
+        admitance = np.zeros((self.n_nodes, self.n_nodes), dtype=np.complex128)
+        for i in range(self.n_nodes):
+            for j in range(self.n_nodes):
+                if R[i, j] != 0 or X[i, j] != 0:
+                    admitance[i, j] = 1 / (R[i, j] + 1j * X[i, j])
+
+        return admitance
+    
+    def get_bus_admitance(self, admitance):
+        '''
+        Get Bus Admitance Matrix
+
+        admitance: Admitance Matrix
+        '''
+        bus_admitance = np.zeros(self.admitance.shape, dtype=np.complex128)
+        for i in range(self.n_nodes):
+            for j in range(self.n_nodes):
+                if i == j:
+                    bus_admitance[i, j] = np.sum(admitance[i, :]) - admitance[i, i]
+                else:
+                    bus_admitance[i, j] = - admitance[i, j]
+
+        bus_admitance = bus_admitance + np.diag(np.sum(admitance, axis=1))
+        return bus_admitance
