04bfb643a107293b3905c73396b9233716a4cf0d
Dani Guijo <dani_guijo@hotmail.com>
2024-02-03 19:07:14 +0100

    Add classes and methods

--- solvers/solvers.py
+++ solvers/solvers.py
@@ -1,2 +1,52 @@
 import numpy as np
 from data.power_system import PowerSystem
+
+
+class Solver():
+    """
+    Class to create a solver.
+    """
+
+    def __init__(self,
+                 tol= 1e-4,
+                 max_iter=10):
+        
+        '''
+        Initializes the solver
+
+        tol: Maximum error to achieve convergence
+        max_iter: Maximum number of iterations of the solver
+        '''
+
+        self.tol = tol
+        self.max_iter = max_iter
+
+    
+class WLS(Solver):
+    '''
+    Class for the Weighted Least Square solver
+    '''
+
+    def solve(self,
+              power_system: PowerSystem,
+              x0: np.array):
+        '''
+        Solves the system
+
+        power_system: PowerSystem object for which we want to estimate the state
+        x0: Initial state
+        '''
+
+        for i in range(self.max_iter):
+            H = power_system.get_H(x)
+            h = power_system.get_h(x)
+            r = power_system.get_residuals(h)
+            G = power_system.get_G(H)
+
+            dx = np.linalg.inv(G) @ H.T @ power_system.W @ r
+            x = x + dx
+
+            if np.all(np.abs(dx) < convergence):
+                break
+
+        return x, r, G, H, h
